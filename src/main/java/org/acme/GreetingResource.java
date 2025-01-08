39a9e16542a48f46a69750ecb272531d99624143
Yanny Diaz <yannyvr6@gmail.com>
2025-01-08 15:37:08 -0700

    h

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,42 @@
 package org.acme;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-@Path("/hello")
-public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Path("/hello")
+    public class GreetingResource {
+
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String hello() { return "Hello RESTEasy";}
+
+        @GET
+        @Path("/personalize/{name}")
+        public String personalizedHello(@PathParam("name") String name) {
+            return "Hello " + name;
     }
+
+    @POST
+    @Path("/personalized")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHelloPost (Person p) {
+            return "Hello " + p.getFirst() + " " + p.getLast();
+    }
+
+    public static class Person{
+            private String first;
+            private String last;
+
+            public String getFirst() {return first; }
+            public void setFirst (String first) {this.first = first; }
+            public String getLast (){return last; }
+            public void setLast (String last) {this.last = last;}
+    }
+
+
 }
