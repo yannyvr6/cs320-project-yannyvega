1f364295fac2ab8e1e3930e79e1e5ca950a6306c
Yanny Diaz <yannyvr6@gmail.com>
2025-01-11 20:46:56 -0700

    h

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import {API_URL} from './config'; //gets the API_URL from the config.js file
 
-function App() {
+function App(){
+  const [message, setMessage] = useState(''); // will hold the message from the backend
+
+  //"useEffect" is a React method that will run after this page is loaded
+  // it will fetch the message from the backend
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const response = await fetch(`${API_URL}/hello/personalized`, {
+        method: 'POST', //POST request
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({first: 'Ensign', last: 'Student'}) //JSON body with attributes for a Person
+      });
+            const text = await response.text(); // waits for the response and then converts it to text
+            setMessage(text); // sets the message with the text from the response
+          };
+      fetchMessage();
+    } , []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Message from the backend: </h1>
+      <p>{message}</p>
     </div>
   );
 }
