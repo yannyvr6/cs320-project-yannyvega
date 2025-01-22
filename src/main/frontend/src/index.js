0350262d0a93732bbdcdc8871d2cbfe57356cad2
Yanny Diaz <yannyvr6@gmail.com>
2025-01-22 14:31:48 -0700

    h

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Page2 from "./Page2";
+import {BrowserRouter , Routes , Route} from "react-router-dom";
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="/page2" element={<Page2/>}>
+            </Route>
+                {/* You can add more pages by adding more rout elements here!*/}
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
